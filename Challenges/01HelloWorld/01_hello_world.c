31edce394dfd8b36a515ef77a32b16292072816b
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-10 13:08:51 -0300

    first commit

---
+++ Challenges/01HelloWorld/01_hello_world.c
@@ -0,0 +1,11 @@
+/* 
+ 1) Write a program that displays the message "Hello, World!" on the screen.
+*/
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World !\n");
+  getchar();
+  return 0;
+}
